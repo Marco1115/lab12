55ad1e7fccd3f126b09812be9ba7fb81a6419317
Marco Battistini <marco.battistini9@studio.unibo.it>
2025-12-14 13:07:52 +0100

    added the logics

---
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -0,0 +1,14 @@
+package it.unibo.es2;
+
+/**
+ * 
+ */
+public interface Logics {
+
+    /**
+     * @param position the cell that has to be hit.
+     * @return true if the cell has been hit.
+     */
+    boolean hit(Pair<Integer, Integer> position);
+
+}
---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,26 @@
+package it.unibo.es2;
+
+/**
+ * 
+ */
+public class LogicsImpl implements Logics {
+
+    /**
+     * Constructor of the logics.
+     * 
+     * @param size the size of the logics.
+     */
+    public LogicsImpl(final int size) {
+        //TODO Auto-generated constructor stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hit(final Pair<Integer, Integer> position) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    }
+
+}
